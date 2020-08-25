8ddd9fbb001395875bd15775dc9e6f3f95df2701
jordelucas <jordevalucas@gmail.com>
2020-08-25 12:17:05 -0300

    adiciona estilo nos dados do perfil

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -17,6 +17,36 @@ const ProfilePage: React.FC = () => {
         <Avatar />
       </Banner>
 
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+        
+        <h1>Jordevá Lucas</h1>
+        <h2>@jordelucas</h2>
+
+        <p>
+          Developer at <a href="https://smartmetropolis.imd.ufrn.br">@SmartMetropolis</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Natal, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 29 de dezembro de 1998
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>537</strong>
+          </span>
+          <span>
+            <strong>1123</strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
     </Container>
   );
 }
--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -40,3 +40,90 @@ export const Avatar = styled.div`
   bottom: max(-60px, -10vw);
   left: 15px;
 `
+
+export const ProfileData = styled.div`
+  padding: min(calc(10vw + 7px), 67px) 16px 0;
+
+  display: flex;
+  flex-direction: column;
+
+  position: relative;
+
+  > h1 {
+    font-weight: bold;
+    font-size: 19px;
+  } 
+
+  > h2 {
+    font-weight: normal;
+    font-size: 15px;
+
+    color: var(--gray);
+  }
+
+  > p {
+    font-size: 15px;
+    margin-top: 11px;
+
+    > a {
+      text-decoration: none;
+      color: var(--twitter)
+    }
+  }
+
+  ul {
+    list-style: none;
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+    > li {
+      font-size: 15px;
+      color: var(--gray);
+
+      > svg {
+        fill: var(--gray);
+        margin-right: 5px;
+      }
+    }
+  }
+`
+
+const iconCSS = css`
+  width: 20px;
+  height: 20px;
+
+  color: var(--gray);
+`
+
+export const LocationIcon = styled(LocationOn)`${iconCSS}`
+export const CakeIcon = styled(Cake)`${iconCSS}`
+
+export const Followage = styled.div`
+  display: flex;
+
+
+  > span {
+    font-size: 15px;
+    color: var(--gray);
+
+    & + span {
+      margin-left: 20px;
+    }
+  }
+`
+
+export const EditButton = styled(Button)`
+  position: absolute;
+  top: 2vw;
+  right: 7px;
+
+  padding: 4px 16px;
+  font-size: 13px;
+
+  @media (min-width: 320px) {
+    top: 10px;
+    padding: 10px 19px;
+
+    font-size: 15px;
+  }
+`
