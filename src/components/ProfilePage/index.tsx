de1f093bc1c24ee3a1c49c38de7851d40ff360aa
jordelucas <jordevalucas@gmail.com>
2020-08-25 12:16:09 -0300

    cria banner da página de perfil

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { 
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton } from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+    </Container>
+  );
+}
+
+export default ProfilePage;
