d3c312ff025aec87f11b8b788b8a24e30dba7a5e
jordelucas <jordevalucas@gmail.com>
2020-08-30 15:13:27 -0300

    corrige scroll do body

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-import { Container, SearchWrapper, SearchInput, SearchIcon, Body } from './styles';
+import StickyBox from 'react-sticky-box'
+
 import List from '../List'
 import FollowSuggestion from '../FollowSuggestion';
 import News from '../News';
+
+import { 
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body } from './styles';
 
 const SideBar: React.FC = () => {
   return (
@@ -13,30 +21,32 @@ const SideBar: React.FC = () => {
         <SearchIcon />
       </SearchWrapper>
 
-      <Body>
-        <List 
-          title="Talvez você curta"
-          elements={[
-            <FollowSuggestion name="Luiz Batanero" nickname="@luizbatanero" />,
-            <FollowSuggestion name="Luke Morales" nickname="@lukemorales" />,
-            <FollowSuggestion name="Camila Magalhães" nickname="@camilaamgl" />
-          ]}
-        />  
+      <StickyBox>
+        <Body>
+          <List 
+            title="Talvez você curta"
+            elements={[
+              <FollowSuggestion name="Luiz Batanero" nickname="@luizbatanero" />,
+              <FollowSuggestion name="Luke Morales" nickname="@lukemorales" />,
+              <FollowSuggestion name="Camila Magalhães" nickname="@camilaamgl" />
+            ]}
+          />  
 
-        <List 
-          title="Talvez você curta"
-          elements={[
-            <News />,
-            <News />,
-            <News />,
-            <News />,
-            <News />,
-            <News />,
-            <News />,
-            <News />
-          ]}
-        />  
-      </Body>
+          <List 
+            title="O que está acontecendo"
+            elements={[
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />,
+              <News />
+            ]}
+          />  
+        </Body>
+      </StickyBox>
     </Container>
   );
 }
